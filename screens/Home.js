8af4291a80f12adf5dbd3697ba0d55c8cb93acd2
Cameron Nye <csnye86@gmail.com>
2021-02-18 14:08:54 -0700

    Added buttons to navigate

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import {globalStyles} from '../styles/Global'
 
-const Home = () => {
+const Home = ({navigation}) => {
+  const {navigate} = navigation
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home</Text>
+      <Button title='Review Details' onPress={() => navigate('ReviewDetails')}/>
     </View>
   )
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import { globalStyles } from '../styles/Global'
 
 
-const ReviewDetails = () => {
+const ReviewDetails = ({navigation}) => {
+  const {goBack} = navigation
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Review Details</Text>
+      <Button title='Back' onPress={() => goBack()}/>
     </View>
   )
 }
