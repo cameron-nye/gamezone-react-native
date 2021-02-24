bd18840d25e8a89f4d55a3dede217906fbb9072b
Cameron Nye <csnye86@gmail.com>
2021-02-24 16:28:25 -0700

    Form works. Adds to list

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
-import { StyleSheet, Modal, Text, View } from "react-native"
-import { FlatList, TouchableOpacity } from "react-native-gesture-handler"
+import { StyleSheet, Modal, Text, View, Keyboard } from "react-native"
+import { FlatList, TouchableOpacity, TouchableWithoutFeedback } from "react-native-gesture-handler"
 import { MaterialIcons } from "@expo/vector-icons"
 import { globalStyles } from "../styles/Global"
 import Card from "../shared/Card"
@@ -9,22 +9,37 @@ import ReviewForm from "./ReviewForm"
 const Home = ({ navigation }) => {
 	const [ modalOpen, setModalOpen ] = useState(false)
 	const [ reviews, setReviews ] = useState([
-		{ title: "League of Legends", rating: 4.5, body: "lorem ipsum", key: "4" },
 		{ title: "Destiny", rating: 5, body: "lorem ipsum", key: "2" },
-		{ title: 'The Last of Us', rating: 5, body: "lorem ipsum", key: "3" }
+		{ title: "The Last of Us", rating: 5, body: "lorem ipsum", key: "3" },
+		{ title: "League of Legends", rating: 4.5, body: "lorem ipsum", key: "4" }
 	])
+
 	const { navigate } = navigation
+
+	const addReview = review => {
+		review.key = Math.random().toString()
+		setReviews(prevState => [ ...prevState, review ])
+		setModalOpen(false)
+	}
 
 	return (
 		<View style={globalStyles.container}>
 			<Modal visible={modalOpen} animationType="slide" style={styles.modalToggle}>
-				<View style={styles.modalContent}>
-					<MaterialIcons name="close" size={24} onPress={() => setModalOpen(false)} style={{...styles.modalToggle, ...styles.modalClose}} />
-					<ReviewForm/>
-				</View>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<View style={styles.modalContent}>
+						<MaterialIcons
+							name="close"
+							size={24}
+							onPress={() => setModalOpen(false)}
+							style={{ ...styles.modalToggle, ...styles.modalClose }}
+						/>
+						<ReviewForm addReview={addReview} />
+					</View>
+				</TouchableWithoutFeedback>
 			</Modal>
 			<MaterialIcons name="add" size={24} onPress={() => setModalOpen(true)} style={styles.modalToggle} />
 			<FlatList
+				style={styles.gameList}
 				data={reviews}
 				renderItem={({ item }) => (
 					<TouchableOpacity onPress={() => navigate("ReviewDetails", item)}>
@@ -40,20 +55,24 @@ const Home = ({ navigation }) => {
 
 const styles = StyleSheet.create({
 	modalContent: {
-    flex: 1,
-  },
+		flex: 1,
+		backgroundColor: "aliceblue"
+	},
 	modalToggle: {
-		marginBottom: 10,
 		borderRadius: 10,
 		borderWidth: 1,
 		borderColor: "gray",
 		padding: 10,
 		paddingHorizontal: 20,
-		alignSelf: "center"
+		alignSelf: "center",
+		backgroundColor: "#fff"
 	},
-  modalClose: {
-    marginTop: 10,
-  }
+	modalClose: {
+		marginTop: 20
+	},
+	gameList: {
+		marginTop: 15
+	}
 })
 
 export default Home
--- screens/ReviewForm.js
+++ screens/ReviewForm.js
@@ -4,7 +4,7 @@ import { globalStyles } from "../styles/Global"
 import { Formik } from "formik"
 import { TextInput } from "react-native-gesture-handler"
 
-const ReviewForm = () => {
+const ReviewForm = ({addReview}) => {
 	return (
 		<View style={globalStyles.container}>
 			<Formik
@@ -13,8 +13,9 @@ const ReviewForm = () => {
 					body: "",
 					rating: ""
 				}}
-				onSubmit={values => {
-					console.log(values)
+				onSubmit={(values, actions) => {
+					addReview(values)
+          actions.resetForm()
 				}}
 			>
 				{props => (
@@ -22,20 +23,20 @@ const ReviewForm = () => {
 						<TextInput
 							style={globalStyles.input}
 							placeholder="Title"
-							onChange={props.handleChange("title")}
+							onChangeText={props.handleChange("title")}
 							value={props.values.title}
 						/>
 						<TextInput
 							style={globalStyles.input}
 							placeholder="Description"
-							onChange={props.handleChange("body")}
+							onChangeText={props.handleChange("body")}
 							value={props.values.body}
 							multiline
 						/>
 						<TextInput
 							style={globalStyles.input}
 							placeholder="Rating (1-5)"
-							onChange={props.handleChange("rating")}
+							onChangeText={props.handleChange("rating")}
 							value={props.values.rating}
 							keyboardType="numeric"
 						/>
